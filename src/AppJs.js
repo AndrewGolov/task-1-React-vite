082893b8bc6458acb96bc20e233cb163cf09b725
AndrewGolov <135165029+AndrewGolov@users.noreply.github.com>
2025-12-04 23:57:49 +0300

    add AppJs javascript code from jsx

---
+++ src/AppJs.js
@@ -0,0 +1,57 @@
+import { createElement, useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '../public/vite.svg';
+import './App.css';
+
+export default function AppJs() {
+	const [count, setCount] = useState(0);
+
+	return createElement(
+		'div',
+		null,
+		createElement(
+			'div',
+			null,
+			createElement(
+				'a',
+				{
+					href: 'https://vite.dev',
+					target: '_blank',
+				},
+				createElement('img', {
+					src: viteLogo,
+					className: 'logo react',
+					alt: 'React logo',
+				}),
+			),
+		),
+		createElement('h1', null, 'Vite + React'),
+		createElement(
+			'div',
+			{
+				className: 'card',
+			},
+			createElement(
+				'button',
+				{
+					onClick: () => setCount((count) => count + 1),
+				},
+				`count is ${count}`,
+			),
+			createElement('p', null, 'Edit', createElement('code', null, 'src/App.jsx'), 'and save to test HMR'),
+		),
+		createElement(
+			'p',
+			{
+				className: 'read-the-docs',
+			},
+			'Click on the Vite and React logos to learn more',
+		),
+		createElement(
+			'p',
+			null,
+			`Текущий год ${new Date().getFullYear()}`
+
+		),
+	);
+}
